c50f8d99a5ffc8387a602b054882f78825709bc9
danielbrugio <daniel.brugioni@hotmail.com>
2022-05-25 19:56:44 -0300

    change

--- serverEntregable.js
+++ serverEntregable.js
@@ -7,10 +7,17 @@ const contenedor = new Contenedor(DBfile)
 
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (request, response) => {
+    response.send(
+      "<h1 style='color: blue'>Hola! Agrega en la url /productos para acceder al listado completo de Productos o /productosRandom para que te muestra un producto al azar</h1>"
+    );
+  });
+
 app.get('/productos', (request, response) => {
  const data = contenedor.getAll()
  response.json(data)
 }); 
+
 app.get('/productosRandom', (request, response) => {
  const data = contenedor.getAll()
  const numero = Math.floor(Math.random() * data.length)
